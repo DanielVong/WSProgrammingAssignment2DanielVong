262b93e33d5807b2a45e4354a56e8d788bd71fd8
DanielVong <danielvong9@gmail.com>
2025-11-01 17:50:50 -0400

    Added partials

--- Assignment2/routes/index.js
+++ Assignment2/routes/index.js
@@ -3,10 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'HomePage' });
+  res.render('index', { title: 'Home' });
 });
 router.get('/home', function(req, res, next) {
-  res.render('index', { title: 'HomePage' });
+  res.render('index', { title: 'Home' });
 });
 router.get('/about', function(req, res, next) {
   res.render('about', { title: 'About Me' });
